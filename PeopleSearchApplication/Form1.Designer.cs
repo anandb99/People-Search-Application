de16a9a133bb6caf88e6e244f9d137221140e8fb
Anand <anandbora99@gmail.com>
2016-10-31 17:47:26 -0600

    Upload

---
+++ PeopleSearchApplication/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace PeopleSearchApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.displayAllButton = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.interestsTextBox = new System.Windows.Forms.TextBox();
+            this.insertButton = new System.Windows.Forms.Button();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.InsertPanel = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.InsertPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(488, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputTextBox.Location = new System.Drawing.Point(545, 39);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(130, 26);
+            this.inputTextBox.TabIndex = 1;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchButton.Location = new System.Drawing.Point(700, 39);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(83, 29);
+            this.searchButton.TabIndex = 2;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // displayAllButton
+            // 
+            this.displayAllButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayAllButton.Location = new System.Drawing.Point(575, 245);
+            this.displayAllButton.Name = "displayAllButton";
+            this.displayAllButton.Size = new System.Drawing.Size(130, 29);
+            this.displayAllButton.TabIndex = 3;
+            this.displayAllButton.Text = "Display All";
+            this.displayAllButton.UseVisualStyleBackColor = true;
+            this.displayAllButton.Click += new System.EventHandler(this.displayAllButton_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(491, 83);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(292, 150);
+            this.dataGridView.TabIndex = 4;
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstNameTextBox.Location = new System.Drawing.Point(169, 57);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(212, 26);
+            this.firstNameTextBox.TabIndex = 5;
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lastNameTextBox.Location = new System.Drawing.Point(169, 83);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(212, 26);
+            this.lastNameTextBox.TabIndex = 6;
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addressTextBox.Location = new System.Drawing.Point(169, 109);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(212, 26);
+            this.addressTextBox.TabIndex = 7;
+            // 
+            // interestsTextBox
+            // 
+            this.interestsTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.interestsTextBox.Location = new System.Drawing.Point(169, 161);
+            this.interestsTextBox.Name = "interestsTextBox";
+            this.interestsTextBox.Size = new System.Drawing.Size(212, 26);
+            this.interestsTextBox.TabIndex = 9;
+            // 
+            // insertButton
+            // 
+            this.insertButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.insertButton.Location = new System.Drawing.Point(108, 203);
+            this.insertButton.Name = "insertButton";
+            this.insertButton.Size = new System.Drawing.Size(200, 29);
+            this.insertButton.TabIndex = 10;
+            this.insertButton.Text = "Insert";
+            this.insertButton.UseVisualStyleBackColor = true;
+            this.insertButton.Click += new System.EventHandler(this.insertButton_Click);
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker.Location = new System.Drawing.Point(169, 135);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(212, 26);
+            this.dateTimePicker.TabIndex = 11;
+            // 
+            // InsertPanel
+            // 
+            this.InsertPanel.Controls.Add(this.label6);
+            this.InsertPanel.Controls.Add(this.insertButton);
+            this.InsertPanel.Controls.Add(this.label5);
+            this.InsertPanel.Controls.Add(this.label4);
+            this.InsertPanel.Controls.Add(this.label3);
+            this.InsertPanel.Controls.Add(this.label2);
+            this.InsertPanel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InsertPanel.Location = new System.Drawing.Point(21, 22);
+            this.InsertPanel.Name = "InsertPanel";
+            this.InsertPanel.Size = new System.Drawing.Size(437, 266);
+            this.InsertPanel.TabIndex = 12;
+            this.InsertPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label6.Location = new System.Drawing.Point(26, 139);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 20);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Interests:";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label5.Location = new System.Drawing.Point(26, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 20);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Date of Birth:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label4.Location = new System.Drawing.Point(26, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Address:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label3.Location = new System.Drawing.Point(26, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Last Name:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label2.Location = new System.Drawing.Point(26, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "First Name:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(465, 22);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(349, 266);
+            this.panel1.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(883, 390);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.interestsTextBox);
+            this.Controls.Add(this.addressTextBox);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.firstNameTextBox);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.displayAllButton);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.InsertPanel);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "People Seach Application";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.InsertPanel.ResumeLayout(false);
+            this.InsertPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Button displayAllButton;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.TextBox interestsTextBox;
+        private System.Windows.Forms.Button insertButton;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.Panel InsertPanel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ PeopleSearchApplication/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace PeopleSearchApplication
+{
+    public partial class Form1 : Form
+    {
+        DataSet ds = new DataSet();
+        SqlConnection cs = new SqlConnection("Data Source=DESKTOP-MML041V\\SQLEXPRESS; Initial Catalog=peopleSearchApplicationDatabase; Integrated Security=TRUE;");
+        SqlDataAdapter da = new SqlDataAdapter();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            //select * from peopleSearchApplicationTable where firstName like 'anand' or lastName like 'reddy'
+            if (inputTextBox.Text != "")
+            {
+                String query = "Select * from peopleSearchApplicationTable where firstName like '" + inputTextBox.Text
+                    + "' or lastName like '" + inputTextBox.Text + "'";
+                Console.WriteLine(query);
+                da.SelectCommand = new SqlCommand(query, cs);
+                ds.Clear();
+                da.Fill(ds);
+                dataGridView.DataSource = ds.Tables[0];
+            }
+            else
+            {
+                MessageBox.Show("Please enter input value");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void displayAllButton_Click(object sender, EventArgs e)
+        {
+            da.SelectCommand = new SqlCommand("select * from peopleSearchApplicationTable",cs);
+            ds.Clear();
+            da.Fill(ds);
+            dataGridView.DataSource = ds.Tables[0];
+        }
+
+        private void insertButton_Click(object sender, EventArgs e)
+        {
+    
+            if (firstNameTextBox.Text == "")
+                MessageBox.Show("Please enter first name");
+            else if (lastNameTextBox.Text == "")
+                MessageBox.Show("Please enter last name");
+            else if (dateTimePicker.Text=="")
+                MessageBox.Show("Please enter DOB");
+            else
+            {
+                da.InsertCommand = new SqlCommand("insert into peopleSearchApplicationTable values (@firstName, @lastName, @address, @dob, @interests)", cs);
+
+                da.InsertCommand.Parameters.Add("@firstName", SqlDbType.VarChar).Value = firstNameTextBox.Text;
+                da.InsertCommand.Parameters.Add("@lastName", SqlDbType.VarChar).Value = lastNameTextBox.Text;
+                da.InsertCommand.Parameters.Add("@address", SqlDbType.VarChar).Value = addressTextBox.Text;
+                da.InsertCommand.Parameters.Add("@dob", SqlDbType.Date).Value = dateTimePicker.Text;
+                da.InsertCommand.Parameters.Add("@interests", SqlDbType.VarChar).Value = interestsTextBox.Text;
+
+                cs.Open();
+                da.InsertCommand.ExecuteNonQuery();
+                cs.Close();
+
+                displayAllButton_Click(null,null);
+            }
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
